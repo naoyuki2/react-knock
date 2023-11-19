8baf3f40818f567683316dae9d21e3efb064a719
naoyuki <kamaboko912@gmail.com>
2023-11-19 15:23:39 +0900

    TODO comp

---
+++ src/component/PlusModal.tsx
@@ -0,0 +1,84 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  useDisclosure,
+  Input,
+  Text,
+} from '@chakra-ui/react'
+import { useState } from 'react'
+import { TodoType } from '../type'
+
+type PlusModalProps = {
+  Todos: TodoType[]
+  setTodos: React.Dispatch<React.SetStateAction<TodoType[]>>
+}
+
+const PlusModal: React.FC<PlusModalProps> = ({ Todos, setTodos }) => {
+  const [title, setTitle] = useState('')
+  const [message, setMessage] = useState('タスク名を入力してください')
+
+  const inputChange = (e: any) => {
+    if (e.target.value === '') {
+      setMessage('タスク名を入力してください')
+      setTitle('')
+    } else {
+      setMessage('')
+      setTitle(e.target.value)
+    }
+  }
+
+  const add = () => {
+    if (!(title === '')) {
+      const newTodo: TodoType = {
+        title: title,
+        done: false,
+      }
+      const newTodos = [...Todos, newTodo]
+      setTodos(newTodos)
+      setTitle('')
+      onClose()
+    }
+  }
+
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  return (
+    <>
+      <Button onClick={onOpen} color="#4C4C4C" bg="#75cbd2">
+        タスクを追加
+      </Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader bg="#75cbd2" color="#4C4C4C">
+            タスク追加
+          </ModalHeader>
+          <ModalBody>
+            <Input
+              placeholder="タスク名を入力"
+              size="lg"
+              value={title}
+              onChange={inputChange}
+            />
+            <Text color="#db4c3a">{message}</Text>
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={add}>
+              追加する
+            </Button>
+            <Button type="submit" variant="ghost" onClick={onClose}>
+              キャンセル
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
+
+export default PlusModal
--- src/component/Todo.tsx
+++ src/component/Todo.tsx
@@ -1,7 +1,80 @@
-import React from 'react'
+import {
+  Card,
+  CardBody,
+  HStack,
+  Text,
+  Center,
+  VStack,
+  Button,
+  Checkbox,
+  CardFooter,
+  CardHeader,
+  Heading,
+} from '@chakra-ui/react'
+import PlusModal from './PlusModal'
+import { TodoType } from '../type'
+import { useState } from 'react'
 
 const Todo = () => {
-  return <div>Todo</div>
+  const initialTodos: TodoType[] = [
+    {
+      title: 'test',
+      done: false,
+    },
+    {
+      title: 'レポート',
+      done: false,
+    },
+  ]
+  const [Todos, setTodos] = useState(initialTodos)
+
+  const deleteTodo = (e: any) => {
+    const newTodos = Todos.filter((todo) => {
+      return todo.title !== e.target.value
+    })
+    setTodos(newTodos)
+  }
+
+  return (
+    <>
+      <Center bg="#e7e7e7" height="100vh">
+        <Card align="center" w="40%" h="500px">
+          <CardHeader
+            w="full"
+            bg="#75cbd2"
+            color="#202124"
+            fontSize="4xl"
+            textAlign="center"
+          >
+            <Heading>Todo List</Heading>
+          </CardHeader>
+          <CardBody>
+            {Todos.map((todo) => (
+              <div key={todo.title}>
+                <HStack justifyContent="space-between">
+                  <Checkbox width="auto">
+                    <Text>{todo.title}</Text>
+                  </Checkbox>
+                  <Button
+                    colorScheme="red"
+                    size="xs"
+                    width="auto"
+                    value={todo.title}
+                    onClick={deleteTodo}
+                  >
+                    ×
+                  </Button>
+                </HStack>
+              </div>
+            ))}
+          </CardBody>
+          <CardFooter>
+            <PlusModal Todos={Todos} setTodos={setTodos} />
+          </CardFooter>
+        </Card>
+      </Center>
+    </>
+  )
 }
 
 export default Todo
